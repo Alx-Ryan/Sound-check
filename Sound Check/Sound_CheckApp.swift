0fd3d85314a40b9b6054ac1fff82157fc0ec347b
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-07-05 18:29:11 -0500

    Project Start
    Project Start

---
+++ Sound Check/Sound_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_CheckApp.swift
+//  Sound Check
+//
+//  Created by Alex Ryan on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Sound_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

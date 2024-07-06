a44c8f3b53ea7872990eefcae163d8ebf049ce82
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-07-06 01:08:45 -0500

    init nav setup

--- Sound Check/ContentView.swift
+++ Sound Check/ContentView.swift
@@ -7,28 +7,56 @@
 
 import SwiftUI
 
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case soundLevels, headphones
+    var id: Self { self }
+
+    var title: String {
+        switch self {
+            case .soundLevels:
+                return "Sound Levels"
+            case .headphones:
+                return "Headphones"
+
+        }
+    }
+}
+
 struct ContentView: View {
+
+    @State private var selectedStat: HealthMetricContext = .soundLevels
+    var isSteps: Bool { selectedStat == .soundLevels }
+
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Sound Levels", systemImage: "waveform")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
 
-                                Text("Avg: Sound")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-
-                            Spacer()
-
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
                         }
+                    }
+                    .pickerStyle(.segmented)
+
+                    VStack {
+                        NavigationLink(value: selectedStat) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Sound Levels", systemImage: "waveform")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+
+                                    Text("Avg: Sound")
+                                        .font(.caption)
+                                }
+
+                                Spacer()
+
+                                Image(systemName: "chevron.right")
+                            }
+                        }
+                        .foregroundStyle(.secondary)
                         .padding(.bottom, 12)
 
                         RoundedRectangle(cornerRadius: 12)
@@ -60,7 +88,11 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                Text(metric.title)
+            }
         }
+        .tint(isSteps ? .pink : .indigo)
     }
 }
 

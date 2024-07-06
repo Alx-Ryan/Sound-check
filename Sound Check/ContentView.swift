74a8008f717ece65cd04656ecb5ae58b9b2df723
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-07-05 19:13:41 -0500

    UI Dashboard roughout

--- Sound Check/ContentView.swift
+++ Sound Check/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Sound Levels", systemImage: "waveform")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+
+                                Text("Avg: Sound")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+
+                            Spacer()
+
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+
+                    VStack(alignment: .leading) {
+                            VStack(alignment: .leading) {
+                                Label("Sound Averages", systemImage: "chart.dots.scatter")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+
+                                Text("Last 28 Days")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                        .padding(.bottom, 12)
+
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 

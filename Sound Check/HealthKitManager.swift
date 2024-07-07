871181d2c5d53628dcf134470c10204eea3fed04
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-07-07 16:21:39 -0500

    mock data for simulator
    intentionally left uncommented code in to delete later.

--- Sound Check/HealthKitManager.swift
+++ Sound Check/HealthKitManager.swift
@@ -14,4 +14,25 @@ import Observation
     let store = HKHealthStore()
 
     let types: Set = [HKQuantityType(.environmentalAudioExposure), HKQuantityType(.headphoneAudioExposure)]
+
+//    func addSimulatorData() async {
+//        var mockSamples: [HKQuantitySample] = []
+//
+//        for i in 0..<28 {
+//            let decibelQuantity = HKQuantity(unit: .decibelAWeightedSoundPressureLevel(), doubleValue: .random(in: 5...110))
+//            //let headphoneQuantity = HKQuantity(unit: .decibelHearingLevel(), doubleValue: .random(in: 5...110))
+//
+//            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+//            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
+//
+//            let environmentSample = HKQuantitySample(type: HKQuantityType(.environmentalAudioExposure) , quantity: decibelQuantity, start: startDate, end: endDate)
+//            let headphoneSample = HKQuantitySample(type: HKQuantityType(.headphoneAudioExposure) , quantity: decibelQuantity, start: startDate, end: endDate)
+//
+//            mockSamples.append(headphoneSample)
+//            mockSamples.append(environmentSample)
+//        }
+//
+//        try! await store.save(mockSamples)
+//        print("Dummy data sent ✅")
+//    }
 }
